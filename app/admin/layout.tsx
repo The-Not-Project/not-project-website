fdead82876ee51f5544662fb717ef6b9e33209c3
Tariq El Ghayate <tarriq@pop-os.fios-router.home>
2026-02-02 22:53:38 -0500

    yeeted admin context provider

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,35 +1,9 @@
 import { auth0 } from '../lib/auth0';
 import { redirect } from 'next/navigation';
-import { AdminServerActionsProvider } from '@/app/contexts/admin-server-actions';
 import NavBar from './shared/components/navbar/navbar.component';
 import { AdminContainer } from './shared/components/layout/layout.styles';
 import Back from './shared/components/backButton/backButton.component';
-import { getUser, updateUser } from '../database/repositories/user.repository';
-import {
-  createCategory,
-  deleteCategory,
-  editCategory,
-  getCategories,
-} from '../database/repositories/category.repository';
-import {
-  createStory,
-  deleteStory,
-  editStory,
-  getHiddenStories,
-  getStories,
-  getStory,
-  republishStory,
-  unpublishStory,
-} from '../database/repositories/story.repository';
-import {
-  addRecommendation,
-  getRecommendations,
-  removeRecommendation,
-} from '../database/repositories/recommendation.repository';
-import {
-  getRadarStory,
-  updateRadarStory,
-} from '../database/repositories/radar.repository';
+
 import "@/app/tiptap/styles/_variables.scss";
 
 
@@ -45,30 +19,8 @@ export default async function AdminLayout({
     redirect("/auth/login");
   }
 
-  const groupedActions = {
-    getUser,
-    updateUser,
-    createCategory,
-    getCategories,
-    editCategory,
-    deleteCategory,
-    createStory,
-    getStories,
-    getStory,
-    editStory,
-    deleteStory,
-    getRecommendations,
-    addRecommendation,
-    removeRecommendation,
-    getRadarStory,
-    updateRadarStory,
-    unpublishStory,
-    republishStory,
-    getHiddenStories
-  };
 
   return (
-    <AdminServerActionsProvider {...groupedActions}>
       <AdminContainer>
         <Back />
         <div className='admin-content'>
@@ -77,6 +29,5 @@ export default async function AdminLayout({
           {children}
         </div>
       </AdminContainer>
-    </AdminServerActionsProvider>
   );
 }
