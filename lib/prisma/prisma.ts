147c6cec13805520c073cba9d33778f8bf7c0ede
Tariq El Ghayate <tarriq@pop-os.fios-router.home>
2026-02-07 21:48:17 -0500

    changed prisma adapter timeout to see if it fixes the build error

--- lib/prisma/prisma.ts
+++ lib/prisma/prisma.ts
@@ -8,7 +8,8 @@ const createPrismaClient = () => {
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASSWORD,
     database: process.env.DATABASE_NAME,
-    connectionLimit: 5
+    connectionLimit: 5,
+    connectTimeout: 10000
   });
   return new PrismaClient({ adapter });
 };
