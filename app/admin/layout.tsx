1012201e5a857ca0c90bcd3b3fb5c41cab13d138
Tarriq <tarekelghayate@gmail.com>
2026-02-26 07:03:35 -0500

    Updated admin page title

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,15 @@
-import { auth0 } from "../../lib/auth0";
+import { Metadata } from "next";
 import { redirect } from "next/navigation";
-import NavBar from "./shared/components/navbar/navbar.component";
+import { auth0 } from "../../lib/auth0";
 import { AdminContainer } from "./shared/components/layout/layout.styles";
+import NavBar from "./shared/components/navbar/navbar.component";
 import Back from "./shared/components/backButton/backButton.component";
 import "@/lib/tiptap/styles/_variables.scss";
 
 export const dynamic = "force-dynamic";
+export const metadata: Metadata = {
+  title: 'Admin | The Not Project',
+};
 
 export default async function AdminLayout({
   children,
